649201bf2d389ad3eee393334ff4a1564857786a
William Duncan <wpdnomad@yahoo.com>
2018-03-11 18:00:32 -0700

    Commit to clear this off and get to work on golf
    site. See
    https://docs.microsoft.com/en-us/aspnet/core/tutorials/razor-pages/model

--- RazorPagesMovie/Startup.cs
+++ RazorPagesMovie/Startup.cs
@@ -21,7 +21,15 @@ namespace RazorPagesMovie
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            
+                // requires 
+                // using RazorPagesMovie.Models;
+                // using Microsoft.EntityFrameworkCore;
+
+                services.AddDbContext<MovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MovieContext")));
+                services.AddMvc();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
